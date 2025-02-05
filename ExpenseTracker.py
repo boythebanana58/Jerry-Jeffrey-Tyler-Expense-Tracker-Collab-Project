41a0d55137dbdf1cc5e3e28eb63f84f11d19605a
ItsJeffCraft <111495613+ItsJeffCraft@users.noreply.github.com>
2025-02-05 11:11:29 -0800

    ExpenseTracker.py

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -1,5 +1,7 @@
+#1 create dictionary of expenses
 expenses = {}
 
+#Function to Add a Task
 def add_expense(name, amount):
     expenses[name] = amount
     print(f"Expense '{name}' added with amount {amount}.")
